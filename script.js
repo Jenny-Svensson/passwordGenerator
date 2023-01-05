9b6dcdf94371cee1f74d7b0e78dd31d6111b7567
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-01-05 20:26:34 +0100

    :art: improve format of the code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const pwEl = document.querySelector('#pw');
-const copyEl = document.querySelector('#copy');
+const pwEl = document.querySelector("#pw");
+const copyEl = document.querySelector("#copy");
 
-const lenEl = document.querySelector('#len');
-const upperEl = document.querySelector('#upper');
-const lowerEl = document.querySelector('#lower');
-const numberEl = document.querySelector('#number');
-const symbolEl = document.querySelector('#symbol');
+const lenEl = document.querySelector("#len");
+const upperEl = document.querySelector("#upper");
+const lowerEl = document.querySelector("#lower");
+const numberEl = document.querySelector("#number");
+const symbolEl = document.querySelector("#symbol");
 
-const generateEl = document.querySelector('#generate');
+const generateEl = document.querySelector("#generate");
 
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
@@ -18,67 +18,66 @@ const symbol = "~!@#$%^&*()_+=|";
 
 // let lowercases be random
 function getLowerCase() {
-    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
 }
 
 // let uppercases be random
 function getUpperCase() {
-    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
 
 // let numbers be random
 function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)];
+  return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
 // let symbols be random
 function getSymbol() {
-    return symbol[Math.floor(Math.random() * symbol.length)];
+  return symbol[Math.floor(Math.random() * symbol.length)];
 }
 
 function generatePassword() {
-    const len = lenEl.value;
-    let password = "";
-    for (let i = 0; i < len; i++) {
-        const checked = generateChecked();
-        password += checked;
-    }
-    pwEl.innerText = password;
+  const len = lenEl.value;
+  let password = "";
+  for (let i = 0; i < len; i++) {
+    const checked = generateChecked();
+    password += checked;
+  }
+  pwEl.innerText = password;
 }
 
 function generateChecked() {
-    const xs = [];
-    if (upperEl.checked) {
-        xs.push(getUpperCase());
-    }
-    if (lowerEl.checked) {
-        xs.push(getLowerCase());
-    }
-    if (numberEl.checked) {
-        xs.push(getNumber());
-    }
-    if (symbolEl.checked) {
-        xs.push(getSymbol());
-    }
-    if (xs.length === 0) return "";
-    return xs[Math.floor(Math.random() * xs.length)];
+  const xs = [];
+  if (upperEl.checked) {
+    xs.push(getUpperCase());
+  }
+  if (lowerEl.checked) {
+    xs.push(getLowerCase());
+  }
+  if (numberEl.checked) {
+    xs.push(getNumber());
+  }
+  if (symbolEl.checked) {
+    xs.push(getSymbol());
+  }
+  if (xs.length === 0) return "";
+  return xs[Math.floor(Math.random() * xs.length)];
 }
 
-    generateEl.addEventListener("click", generatePassword);
+generateEl.addEventListener("click", generatePassword);
 
-    copyEl.addEventListener("click", copyPassword);
-
+copyEl.addEventListener("click", copyPassword);
 
 function copyPassword() {
-    const textarea = document.createElement("textarea");
-    const password = pwEl.innerText;
-    if (!password) {
-        return;
-    }
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-    alert('copied!');
-    };
+  const textarea = document.createElement("textarea");
+  const password = pwEl.innerText;
+  if (!password) {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("copied!");
+}
